c35378c492babbb640ef031146dc26e2e65408fd
Danielandro <mtme13@hotmail.com>
2019-06-25 10:12:40 +0100

    Refactor isDivisbleBy method

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -8,21 +8,21 @@ describe('Javabuzz', function () {
 
   describe('knows when a number is', function() {
     it('divisible by 3', function() {
-      expect(javabuzz.isDivisibleByThree(3)).toBe(true);
+      expect(javabuzz.isDivisibleBy(6, 3)).toBe(true);
     });  
     
     it('divisible by 5', function() {
-      expect(javabuzz.isDivisibleByFive(5)).toBe(true);
+      expect(javabuzz.isDivisibleBy(10, 5)).toBe(true);
     });
 
   });
   describe('knows when a number is NOT', function() {
     it('divisible by 3', function () {
-      expect(javabuzz.isDivisibleByThree(4)).toBe(false);
+      expect(javabuzz.isDivisibleBy(4, 3)).toBe(false);
     });
 
     it('divisible by five', function() {
-      expect(javabuzz.isDivisibleByFive(6)).toBe(false);
+      expect(javabuzz.isDivisibleBy(6, 5)).toBe(false);
     });
   });
 
